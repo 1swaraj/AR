6fb46cdd5ea62d39c3bedd9b13d9e82801ac78a3
root <root@localhost.localdomain>
2019-04-21 12:09:34 +0530

    first commit

---
+++ Assets/show.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class show : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject p;
+    public GameObject a;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void aable()
+    {
+        p.SetActive(true);
+    }
+    public void sable()
+    {
+        a.SetActive(true);
+    }
+    public void di()
+    {
+        p.SetActive(false);
+        a.SetActive(false);
+    }
+
+    public void screens()
+    {
+        ScreenCapture.CaptureScreenshot("SomeLevel"+Random.Range(1,10)+".png");
+    }
+}
